e30dfcace1d1ebf3e0fe273584ff47c134a34440
06shelk <shlee4349@gmail.com>
2023-11-09 03:47:31 +0900

    ArtistInformation 추가

---
+++ ArtistInformation.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Onedayasong</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/artistInformation.css">
+</head>
+<body>
+    <header>
+        <nav class="nav-container">
+
+            <div class="nav-toggle" id="nav_toggle">
+                <i class="bi bi-list"></i>
+            </div>
+
+            <ul class="nav-list">
+                <li class="nav-item"><a href="OneSongADay.html" class="nav-link">하루한곡</a></li>
+                <li class="nav-item"><a href="musicCategory.html" class="nav-link">음악 카테고리</a></li>
+                <li class="nav-item"><a href="board.html" class="nav-link">커뮤니티</a></li>
+                <li class="nav-item"><a href="Settings.html" class="nav-link">설정</a></li>
+            </ul>
+            
+        </nav>
+    </header>
+
+    <main id="review">
+
+        <div class="title-container">
+            <div class="Artist">
+                <h1>아티스트 정보</h1>
+            </div>
+        </div>
+
+        <div class="image-info-groups container">
+            <div class="profilecontainer">
+                <div class="userPhoto">
+                    <div class="singer">  
+                        .singer
+                    </div>
+                </div>
+                <div class="userProfile">
+                    <img src="" alt="">
+                </div>
+            </div>
+
+            <div class="info-groups container">
+                <div class="info-group container">
+                    <div class="info container">
+                        <div class="title-text">제목 :</div>
+                        <div class="debut-text">데뷔일 :</div>
+                    </div>
+                    <div class="info container">
+                        <div class="agency-text">소속사 :</div>
+                        <div class="activity-text">팀구성 :</div>
+                    </div>
+                </div>
+
+                <div class="info-group container">
+                    <div class="info container">
+                        <div class="title"></div>
+                        <div class="debut"></div>
+                    </div>
+                    <div class="info container">
+                        <div class="agency"></div>
+                        <div class="activity"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="columns-container">
+            <div class="division-container">
+                <button div class="musicByDate"><h3>아티스트 음악</h3></div>
+            </div>
+        </div>
+            
+        <div class="column-container">
+            <div class="items-container">
+                <div class="CategoryCover">
+                    <img src="" alt="">
+                </div>
+                <div class="CategoryCover">
+                    <img src="" alt="">
+                </div>
+                <div class="CategoryCover">
+                    <img src="" alt="">
+                </div>
+                <div class="CategoryCover">
+                    <img src="" alt="">
+                </div>
+            </div>
+            <div class="items-container">
+                <div class="CategoryTitle"></div>
+                <div class="CategoryTitle"></div>
+                <div class="CategoryTitle"></div>
+                <div class="CategoryTitle"></div>
+            </div>
+        </div>
+
+    </main>
+    <script src="js/test.js"></script>
+    <script src="js/nav.js"></script>
+    <script src="js/artistinformation.js"></script>
+    
+</body>
+</html>
