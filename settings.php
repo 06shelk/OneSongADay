7c5063715ce4d896a95d6288cc87053ef2052ccb
06shelk <shlee4349@gmail.com>
2023-11-17 23:20:52 +0900

    찐찐찐찐 최종

--- settings.php
+++ settings.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Onedayasong</title>
+    <title>하루한곡</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/nav.css">
